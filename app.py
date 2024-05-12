26ea5dc5bf9755740fc8663a29168fb21da8f3f9
raoshun <s.hirao.767@outlook.jp>
2024-05-12 23:46:21 +0900

    configure dashboard layout

--- app.py
+++ app.py
@@ -5,25 +5,73 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+sidebar = html.Div(
+    [
+        dbc.Row(
+            [
+                html.P('Settings')
+                ],
+            style={"height": "5vh"}, className='bg-primary text-white'
+            ),
+        dbc.Row(
+            [
+                html.P('Categorical and Continuous Variables')
+                ],
+            style={"height": "50vh"}, className='bg-secondary text-white'
+            ),
+        dbc.Row(
+            [
+                html.P('Target Variables')
+                ],
+            style={"height": "45vh"}, className='bg-dark text-white'
+            )
+        ]
+)
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+content = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.P('Distribution of Categorical Variable'),
+                        ],
+                    className='bg-white'
+                    ),
+                dbc.Col(
+                    [
+                        html.P('Distribution of Continuous Variable')
+                    ],
+                    className='bg-dark text-white'
+                    )
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+            style={"height": "50vh"}),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.P('Correlation Matrix Heatmap')
+                    ],
+                    className='bg-light'
+                    )
+            ],
+            style={"height": "50vh"}
+            )
+        ]
+)
+
+app.layout = app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(sidebar, width=3, className='bg-light'),
+                dbc.Col(content, width=9)
+                ],
+            style={"height": "100vh"}
+            ),
+        ],
+    fluid=True
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0')

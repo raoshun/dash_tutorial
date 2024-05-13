aa72a282ddcfc1ddb29a5fff5726b8c698b97412
raoshun <s.hirao.767@outlook.jp>
2024-05-14 01:14:09 +0900

    add n_samples piechart

--- app.py
+++ app.py
@@ -3,30 +3,87 @@ import dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
 
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+df = pd.read_csv('data/data_sample.csv')
+vars_cat = [var for var in df.columns if var.startswith('cat')]
+vars_cont = [var for var in df.columns if var.startswith('cont')]
+
+pie = df.groupby('target').count()['id'] / len(df)
+
+fig_pie = go.Figure(
+    data=[go.Pie(labels=list(pie.index),
+                 values=pie.values,
+                 hole=.3,
+                 marker=dict(colors=['#bad6eb', '#2b7bba']))])
+
+fig_pie.update_layout(
+    width=320,
+    height=250,
+    margin=dict(l=30, r=10, t=10, b=10),
+    paper_bgcolor='rgba(0,0,0,0)',
+)
+
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
 sidebar = html.Div(
     [
         dbc.Row(
             [
-                html.P('Settings')
+                html.H5('Settings',
+                        style={'margin-top': '12px', 'margin-left': '24px'})
                 ],
-            style={"height": "5vh"}, className='bg-primary text-white'
+            style={"height": "5vh"},
+            className='bg-primary text-white font-italic'
             ),
         dbc.Row(
             [
-                html.P('Categorical and Continuous Variables')
+                html.Div([
+                    html.P('Categorical Variable',
+                        style={'margin-top': '8px', 'margin-bottom': '4px'},
+                        className='font-weight-bold'),
+                    dcc.Dropdown(id='my-cat-picker', multi=False, value='cat0',
+                                options=[{'label': x, 'value': x}
+                                        for x in vars_cat],
+                                style={'width': '320px'}
+                                ),
+                    html.P('Continuous Variable',
+                        style={'margin-top': '16px', 'margin-bottom': '4px'},
+                        className='font-weight-bold'),
+                    dcc.Dropdown(id='my-cont-picker', multi=False, value='cont0',
+                                options=[{'label': x, 'value': x}
+                                        for x in vars_cont],
+                                style={'width': '320px'}
+                                ),
+                    html.P('Continuous Variables for Correlation Matrix',
+                        style={'margin-top': '16px', 'margin-bottom': '4px'},
+                        className='font-weight-bold'),
+                    dcc.Dropdown(id='my-corr-picker', multi=True,
+                                value=vars_cont + ['target'],
+                                options=[{'label': x, 'value': x}
+                                        for x in vars_cont + ['target']],
+                                style={'width': '320px'}
+                                ),
+                    html.Button(id='my-button', n_clicks=0, children='apply',
+                                style={'margin-top': '16px'},
+                                className='bg-dark text-white'),
+                    html.Hr()
+                    ]
+                    )
                 ],
-            style={"height": "50vh"}, className='bg-secondary text-white'
-            ),
+            style={'height': '50vh', 'margin': '8px'}),
         dbc.Row(
             [
-                html.P('Target Variables')
+                html.P('Target Variables', className='font-weight-bold'),
+                dcc.Graph(figure=fig_pie)
                 ],
-            style={"height": "45vh"}, className='bg-dark text-white'
+            style={"height": "45vh", 'margin': '8px'}
             )
         ]
-)
+    )
 
 content = html.Div(
     [
